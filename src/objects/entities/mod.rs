cbcc7871ce8b6666f32a879b53db38986d0e4c84
Felix <fedecraft3312@gmail.com>
2024-06-15 21:02:58 -0300

    Moved heavy stuff to entity bundle

--- src/objects/entities/mod.rs
+++ src/objects/entities/mod.rs
@@ -1,18 +1,87 @@
 use bevy::prelude::*;
-
-use crate::properties;
+use bevy::ecs::component::Component;
+use bevy::render::view::RenderLayers;
 
 pub mod player;
 
-use properties::{
-    Position, Velocity
+use crate::FOREGROUND;
+use crate::properties::{
+    Name,
+    Position,
+    Velocity,
+    Hitbox,
+    Speed,
 };
 
-/// Game entity that can move
-/// 
-/// 
 #[derive(Component)]
 pub struct Entity;
+
+#[derive(Bundle)]
+pub struct EntityBundle {
+    // Identification
+    pub entity: Entity,
+    
+    // Visual name
+    pub name: Name,
+    
+    // Physics and other things
+    pub position: Position,
+    pub velocity: Velocity,
+    pub hitbox: Hitbox,
+    pub speed: Speed,
+    
+    // Rendering
+    pub layer: RenderLayers,
+    pub sprite: SpriteBundle,
+}
+
+/// Convert name to name id
+/// 
+/// 'name_id' will often times be the same as 'name' lowercase and with underscores for spaces
+pub fn name_id(name: &str) -> String {
+    let mut name_id = name.to_string();
+    name_id = name_id.replace(" ", "_");
+    name_id = name_id.to_lowercase();
+    return name_id;
+}
+
+/// Constructors
+/// 
+/// 
+impl EntityBundle {
+    /// Standard
+    /// 
+    /// 
+    pub fn new(
+        asset_server: Res<AssetServer>,
+        name: &str
+    ) -> EntityBundle {
+        let name_id = name_id(name);
+        let sprite_path = format!("sprite/entities/{name_id}/{name_id}.png");
+        let sprite = SpriteBundle {
+            transform: Transform::from_xyz(0., 0., 0.),
+            texture: asset_server.load(sprite_path),
+            ..default()
+        };
+        
+        EntityBundle {
+            // Identification
+            entity: Entity,
+            
+            // Visual name
+            name: Name(name.to_string()),
+            
+            position: Position(Vec2::new(0., 0.)),
+            velocity: Velocity(Vec2::new(0., 0.)),
+            hitbox: Hitbox(Vec2::new(32., 64.)),
+            speed: Speed(5.),
+            
+            // Rendering
+            layer: FOREGROUND,
+            sprite
+        }
+    }
+}
 
 /// Move entities
 /// 
--- src/objects/entities/player.rs
+++ src/objects/entities/player.rs
@@ -1,16 +1,11 @@
 use bevy::prelude::*;
 use bevy::ecs::{component::Component, system::Commands};
-use bevy::render::view::RenderLayers;
 
-use crate::FOREGROUND;
 use crate::properties::{
-    Name,
-    Position,
     Velocity,
-    Hitbox,
     Speed,
 };
-use super::Entity;
+use super::EntityBundle;
 
 #[derive(Component)]
 pub struct Player;
@@ -18,60 +13,9 @@ pub struct Player;
 #[derive(Bundle)]
 pub struct PlayerBundle {
     // Identification
-    pub entity: Entity,
+    pub entity_bundle: EntityBundle,
     pub player: Player,
     
-    // Visual name
-    pub name: Name,
-    
-    pub position: Position,
-    pub velocity: Velocity,
-    pub hitbox: Hitbox,
-    pub speed: Speed,
-    
-            // Rendering
-    pub layer: RenderLayers,
-    pub sprite: SpriteBundle,
-}
-
-/// Constructors
-/// 
-/// 
-impl PlayerBundle {
-    /// Standard
-    /// 
-    /// 
-    pub fn new(
-        asset_server: Res<AssetServer>,
-        name: &str
-    ) -> PlayerBundle {
-        // Is it possible to load jpg?, most blocks don't have transparency.
-        let name_id = "player";
-        let sprite_path = format!("sprite/entities/{name_id}/{name_id}.png");
-        let sprite = SpriteBundle {
-            transform: Transform::from_xyz(0., 0., 0.),
-            texture: asset_server.load(sprite_path),
-            ..default()
-        };
-        
-        PlayerBundle {
-            // Identification
-            entity: Entity,
-            player: Player,
-            
-            // Visual name
-            name: Name(name.to_string()),
-            
-            position: Position(Vec2::new(0., 0.)),
-            velocity: Velocity(Vec2::new(0., 0.)),
-            hitbox: Hitbox(Vec2::new(32., 64.)),
-            speed: Speed(5.),
-            
-            // Rendering
-            layer: FOREGROUND,
-            sprite: sprite
-        }
-    }
 }
 
 /// Spawn a player
@@ -82,10 +26,13 @@ pub fn spawn_player(
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn(
-        PlayerBundle::new(
-            asset_server,
-            "Player"
-        )
+        PlayerBundle {
+            entity_bundle: EntityBundle::new(
+                asset_server,
+                "Player"
+            ),
+            player: Player,
+        }
     );
 }
 
