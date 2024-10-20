aca9917132d5a124b293827649f60cb98380fba8
Felix <fedecraft3312@gmail.com>
2024-10-20 19:02:41 -0300

    It seems like bevy inspector just won't work on my pc, anyways

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,32 @@
-use bevy::{
-    app::App, prelude::*, DefaultPlugins,
-    render::view::RenderLayers,
-};
+use bevy::{app::App, prelude::*, render::view::RenderLayers, DefaultPlugins};
+use bevy_egui::EguiPlugin;
 use bevy_rapier2d::prelude::*;
 
-pub mod objects;
 pub mod camera;
+pub mod objects;
 pub mod plugin;
+pub mod properties;
 pub mod state_transition;
 pub mod states;
-pub mod properties;
 pub mod tutorial;
 pub mod ui;
 
 use objects::{
     block,
-    entities::player::{
-        self,
-        handle_player_input
-    },
+    entities::player::{self, handle_player_input},
 };
 
 // const BACKGROUND: RenderLayers = RenderLayers::layer(1);
 const FOREGROUND: RenderLayers = RenderLayers::layer(2);
 
-use states::{
-    AppState,
-    GameMode,
-    PauseState,
-    LoadingState,
-    InGameState,
-};
+use states::{AppState, GameMode, InGameState, LoadingState, PauseState};
 
 /// Main
-/// 
-/// 
+///
+///
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(
-            ImagePlugin::default_nearest()
-        ))
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(32.))
         .add_plugins(RapierDebugRenderPlugin::default())
         // Add state to app definition
@@ -48,28 +35,28 @@ fn main() {
         .init_state::<PauseState>()
         .init_state::<LoadingState>()
         .init_state::<InGameState>()
-        .add_systems(Startup, (
-            camera::initialize_camera,
-            block::spawn_grass_block,
-            player::spawn_player,
-            block::move_blocks.after(block::spawn_grass_block)
-        ))
-        // .add_systems(Startup, (
-        //     (camera::initialize_camera,),
-        //     (block::spawn_grass_block,),
-        //     (player::spawn_player,),
-        //     (block::move_blocks.after(block::spawn_grass_block),)
-        // ))
-        .add_systems(Update, (
-            handle_player_input,
-        ))
+        .add_systems(
+            Startup,
+            (
+                camera::initialize_camera,
+                block::spawn_grass_block,
+                player::spawn_player,
+                block::move_blocks.after(block::spawn_grass_block),
+            ),
+        )
+        .add_systems(Update, (handle_player_input,))
         // Update states
         // We can add systems to trigger during transitions
         .add_systems(OnEnter(AppState::MainMenu), state_transition::main_menu)
         // Set pause state to running when the player is playing
         .add_systems(OnEnter(PauseState::Running), state_transition::play_game)
-        .add_systems(OnEnter(AppState::SettingsMenu), state_transition::open_settings_menu)
-        .add_systems(OnEnter(InGameState::InGameMenu), state_transition::in_game_menu)
+        .add_systems(
+            OnEnter(AppState::SettingsMenu),
+            state_transition::open_settings_menu,
+        )
+        .add_systems(
+            OnEnter(InGameState::InGameMenu),
+            state_transition::in_game_menu,
+        )
         .run();
-    
 }
